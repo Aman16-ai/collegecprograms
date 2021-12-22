4f5efbd8b5e5527ec2dcc43f8a4cd9d2bec2379b
Aman saxena <74807466+Aman16-ai@users.noreply.github.com>
2021-12-22 06:56:42 +0000

    add new program

---
+++ matrixoperations.c
@@ -0,0 +1,115 @@
+#include<stdio.h>
+#include<stdlib.h>
+int mat1[20][20],mat2[20][20],resultmat[20][20];
+int row,col,row2,col2;
+int menu() {
+  int choice;
+  printf("\nPress 1.Addition of matrices");
+  printf("\nPress 2.Subtraction of matrices");
+  printf("\nPress 3.Upper and lower triangle of matrix");
+  printf("\nPress 4.Multpication of matrices");
+  printf("\nPress 5.Transpose of matrix");
+  printf("\nPress 6.Exit");
+  printf("\n\nEnter your choice");
+  scanf("%d",&choice);
+  return choice;
+}
+int takematinputforAdditionAndSub() {
+  int input_flag = 0;
+  printf("\nEnter the row and column of matrix 1");
+  scanf("%d%d",&row,&col);
+  printf("\nEnter the row and column of matrix 2");
+  scanf("%d%d",&row2,&col2);
+  if(row == row2 && col == col2) {
+    input_flag = 1;
+     printf("\n\nEnter the values of 1st matrix");
+  for(int i = 0;i<row;i++) {
+    for(int j = 0;j<col;j++) {
+      scanf("%d",&mat1[i][j]);
+    }
+  }
+  printf("\n\nEnter the values of 2nd matrix");
+  for(int i = 0;i<row;i++) {
+    for(int j = 0;j<col;j++) {
+      scanf("%d",&mat2[i][j]);
+    }
+  }
+    return input_flag;
+  }
+  else {
+    return input_flag;
+  }
+}
+void displayMatrixvalues() {
+printf("\n\nvalues of mat1\n");
+  for(int i = 0;i<row;i++) {
+    for(int j = 0;j<col;j++) {
+      printf(" %d ",mat1[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n\nvalues of mat2\n");
+  for(int i = 0;i<row2;i++) {
+    for(int j = 0;j<col2;j++) {
+      printf(" %d ",mat2[i][j]);
+    }
+    printf("\n");
+  }
+}
+void addMatrices() {
+  int flag = takematinputforAdditionAndSub();
+  if(flag == 1) {
+    displayMatrixvalues();
+  for(int i =0;i<row;i++) {
+    for(int j = 0;j<col;j++) {
+      resultmat[i][j] = mat1[i][j] + mat2[i][j];
+    }
+  }
+  printf("\n\nAddition of mat1 and mat2 is \n");
+  for(int i =0;i<row;i++) {
+    for(int j = 0;j<col;j++) {
+      printf(" %d ",resultmat[i][j]);
+    }
+    printf("\n");
+  }
+  }
+  else {
+    printf("\n\nRow and column must be same for addition\n\n");
+  }
+}
+void subtractMatrices() {
+  int flag = takematinputforAdditionAndSub();
+  if(flag == 1) {
+    displayMatrixvalues();
+  for(int i =0;i<row;i++) {
+    for(int j = 0;j<col;j++) {
+      resultmat[i][j] = mat1[i][j] - mat2[i][j];
+    }
+  }
+  printf("\n\nSubtraction of mat1 and mat2 is \n");
+  for(int i =0;i<row;i++) {
+    for(int j = 0;j<col;j++) {
+      printf(" %d ",resultmat[i][j]);
+    }
+    printf("\n");
+  }
+  }
+  else {
+    printf("\n\nRow and column must be same for subtraction\n\n");
+  }
+}
+int main() {
+  while(1) {
+    switch(menu()) {
+      case 1:
+      addMatrices();
+      break;
+      case 2:
+      subtractMatrices();
+      break;
+      case 6:
+      exit(0);
+    }
+  }
+  return 0;
+}
